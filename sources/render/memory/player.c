276f2a3ecd8b333e4a982a4ae46f6ca5e29b136c
Ricardo Moreira <devrmoreira@gmail.com>
2025-11-26 14:24:33 +0000

    Implemented cardinal starting direction

--- sources/render/memory/player.c
+++ sources/render/memory/player.c
@@ -6,7 +6,7 @@
 /*   By: rimagalh <rimagalh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 11:20:27 by rimagalh          #+#    #+#             */
-/*   Updated: 2025/11/26 11:57:15 by rimagalh         ###   ########.fr       */
+/*   Updated: 2025/11/26 14:23:52 by rimagalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	is_player(char c)
 	return (c == 'N' || c == 'E' || c == 'S' || c == 'W');
 }
 
-void	get_player_pos(t_player *plyr, char **map)
+char	get_player_pos(t_player *plyr, char **map)
 {
 	int	y;
 	int	x;
@@ -32,11 +32,66 @@ void	get_player_pos(t_player *plyr, char **map)
 			{
 				plyr->posX = x + 0.5;
 				plyr->posY = y + 0.5;
-				return ;
+				return (map[y][x]);
 			}
 			x++;
 		}
 		y++;
+	}
+	return (0);
+}
+
+void	set_plane(t_player *plyr, char **map)
+{
+	char	c;
+
+	c = get_player_pos(plyr, map);
+	if (c == 'N')
+	{
+		plyr->plnX = 0.66;
+		plyr->plnY = 0;
+	}
+	else if (c == 'E')
+	{
+		plyr->plnX = 0;
+		plyr->plnY = 0.66;
+	}
+	else if (c == 'S')
+	{
+		plyr->plnX = -0.66;
+		plyr->plnY = 0;
+	}
+	else if (c == 'W')
+	{
+		plyr->plnX = 0;
+		plyr->plnY = -0.66;
+	}
+}
+
+void	set_direction(t_player *plyr, char **map)
+{
+	char	c;
+
+	c = get_player_pos(plyr, map);
+	if (c == 'N')
+	{
+		plyr->dirX = 0;
+		plyr->dirY = -1;
+	}
+	else if (c == 'E')
+	{
+		plyr->dirX = -1;
+		plyr->dirY = 0;
+	}
+	else if (c == 'S')
+	{
+		plyr->dirX = 0;
+		plyr->dirY = 1;
+	}
+	else if (c == 'W')
+	{
+		plyr->dirX = 1;
+		plyr->dirY = 0;
 	}
 }
 
@@ -46,10 +101,8 @@ void	ft_init_player(t_game *game, char **map)
 	if (!game->plyr)
 		return ;
 	get_player_pos(game->plyr, map);
-	game->plyr->dirX = -1;
-	game->plyr->dirY = 0;
-	game->plyr->plnY = 0.66;
-	game->plyr->plnX = 0;
+	set_direction(game->plyr, map);
+	set_plane(game->plyr, map);
 	game->plyr->moveSpeed = 0.05;
 	game->plyr->rotSpeed = 0.03;
 }
